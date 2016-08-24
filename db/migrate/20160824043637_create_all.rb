895da3d4e4a8dac24c99eb57e60c7c8c57a35509
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-24 14:53:43 +1000

    Migrations v.1.1

--- db/migrate/20160824043637_create_all.rb
+++ db/migrate/20160824043637_create_all.rb
@@ -1,16 +1,7 @@
 class CreateAll < ActiveRecord::Migration
   def change
-  	create_table :posts do |t|
-  		t.text :content
-  		t.text :author
-  		t.timestamps
-  	end
 
-  	create_table :comment do |t|
-  		t.belongs_to :post, index: true
-  		t.text :commtext
-  		t.text :cauthor
-  		t.timestamps
-  	end
+
+
   end
 end
