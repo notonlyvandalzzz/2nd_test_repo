146d848c73f5c3e1ba1053b0aa92952d95a84bc9
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-24 16:53:42 +1000

    1.3

--- app.rb
+++ app.rb
@@ -123,7 +123,7 @@ get '/comms/:id' do
     # post_id = params[:id]
     # @currpost = @db.execute 'select * from Posts where id=?', [post_id]
     # @allcomments = @db.execute 'select * from Comms where pid=? order by id desc', [post_id]
-    @c_post = Posts.find(params[:id])
+    @c_post = Post.find(params[:id])
     @cmnts = @c_post.comments
     erb :comments
 end
