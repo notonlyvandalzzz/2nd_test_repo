604111a59dc3e4967da0bdcb9ff4c8137bb3c308
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-24 16:22:24 +1000

    Migr mod

--- db/migrate/20160824061859_create_comments.rb
+++ db/migrate/20160824061859_create_comments.rb
@@ -1,4 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
+  	create_table :comments do |t|
+  		t.belongs_to :post, index: true
+  		t.text :commtext
+  		t.text :cauthor
+  		t.timestamps
+  	end
   end
 end
